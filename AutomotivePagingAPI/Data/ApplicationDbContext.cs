215b78864d31e003a54c7cdbe46e4844c8d25201
Gerson Azabache <gerson_dunk@hotmail.com>
2022-10-23 19:14:12 -0500

    entity added, connection string configured and application context configured

---
+++ AutomotivePagingAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using AutomotivePagingAPI.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomotivePagingAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        //Entities
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
---
+++ AutomotivePagingAPI/Entities/Cliente.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AutomotivePagingAPI.Entities
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Nombre { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+    }
+}
