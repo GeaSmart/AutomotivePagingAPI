8ab65d415668ccdb5f23ba23f96c6e450cd1a3fa
Gerson Azabache <gerson_dunk@hotmail.com>
2022-10-23 19:24:20 -0500

    first endpoint added

---
+++ AutomotivePagingAPI/Controllers/ClientesController.cs
@@ -0,0 +1,25 @@
+﻿using AutomotivePagingAPI.Data;
+using AutomotivePagingAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutomotivePagingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+
+        public ClientesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        [HttpGet]
+        public async Task<List<Cliente>> Get()
+        {
+            return await context.Clientes.ToListAsync();
+        }
+    }
+}
